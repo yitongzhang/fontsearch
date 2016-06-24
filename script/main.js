fea8370675073276a984dc85be4e8bc62327eeeb
Yitong <zha.yitong@gmail.com>
2016-06-24 12:40:32 -0500

    updated png to svg

--- script/main.js
+++ script/main.js
@@ -8,21 +8,21 @@ console.log(compareItems.length)
 
 if(compareItems.length==0) {
 	console.log("0 item!")
-	$(".compareTray").css("background","url(img/tray0.png) no-repeat")
+	$(".compareTray").css("background","url(img/compareTray0.png) no-repeat")
 	$(".compare button").css("display","none")
 } 
 else{
 
 	if(compareItems.length==1){
 		console.log("1 item!")
-		$(".compareTray").css("background","url(img/tray1.png) no-repeat")
+		$(".compareTray").css("background","url(img/compareTray1.png) no-repeat")
 		// $(".compareTray").css("opacity","0")
 		$(".compare button").css("display","none")
 	}
 
 	else{
 		console.log("2 item!")
-		$(".compareTray").css("background","url(img/tray2.png) no-repeat")
+		$(".compareTray").css("background","url(img/compareTray2.png) no-repeat")
 		$(".compare button").css("display","block")
 	}
 
@@ -173,30 +173,30 @@ var flatClick =0;
 $(".serifFonts img").click(function() {
 	if (serifFontsClick%2 == 0){
 
-	 	serifpic.attr("src","img/serif.png")
-	 	sanspic.attr("src","img/sansLight.png")
-	 	slabpic.attr("src","img/slabLight.png")
+	 	serifpic.attr("src","img/serif.svg")
+	 	sanspic.attr("src","img/sansLight.svg")
+	 	slabpic.attr("src","img/slabLight.svg")
 		sansFontsClick =0;
 		slabFontsClick =0;
 	}
 
 	else{
-	 	serifpic.attr("src","img/serifLight.png")
+	 	serifpic.attr("src","img/serifLight.svg")
 	}
 	serifFontsClick++;
 });
 
 $(".sansFonts img").click(function() {
 	if (sansFontsClick%2 == 0){
-	 	sanspic.attr("src","img/sans.png")
-	 	serifpic.attr("src","img/serifLight.png")
-	 	slabpic.attr("src","img/slabLight.png")
+	 	sanspic.attr("src","img/sans.svg")
+	 	serifpic.attr("src","img/serifLight.svg")
+	 	slabpic.attr("src","img/slabLight.svg")
 		serifFontsClick =0;
 		slabFontsClick =0;
 	}
 
 	else{
-	 	sanspic.attr("src","img/sansLight.png")
+	 	sanspic.attr("src","img/sansLight.svg")
 	}
 	sansFontsClick++;
 });
@@ -204,41 +204,41 @@ $(".sansFonts img").click(function() {
 
 $(".slabFonts img").click(function() {
 	if (slabFontsClick%2 == 0){
-	 	slabpic.attr("src","img/slab.png")
-	 	serifpic.attr("src","img/serifLight.png")
-	 	sanspic.attr("src","img/sansLight.png")
+	 	slabpic.attr("src","img/slab.svg")
+	 	serifpic.attr("src","img/serifLight.svg")
+	 	sanspic.attr("src","img/sansLight.svg")
 		sansFontsClick =0;
 		serifFontsClick =0;
 	}
 
 	else{
-	 	slabpic.attr("src","img/slabLight.png")
+	 	slabpic.attr("src","img/slabLight.svg")
 	}
 	slabFontsClick++;
 });
 
 $(".largeBrackets img").click(function() {
 	if (largeClick%2 == 0){
-	 	largepic.attr("src","img/large.png")
-	 	smallpic.attr("src","img/smallLight.png")
+	 	largepic.attr("src","img/large.svg")
+	 	smallpic.attr("src","img/smallLight.svg")
 		smallClick =0;
 	}
 
 	else{
-	 	largepic.attr("src","img/largeLight.png")
+	 	largepic.attr("src","img/largeLight.svg")
 	}
 	largeClick++;
 });
 
 $(".smallBrackets img").click(function() {
 	if (smallClick%2 == 0){
-	 	smallpic.attr("src","img/small.png")
-	 	largepic.attr("src","img/largeLight.png")
+	 	smallpic.attr("src","img/small.svg")
+	 	largepic.attr("src","img/largeLight.svg")
 		largeClick =0;
 	}
 
 	else{
-	 	smallpic.attr("src","img/smallLight.png")
+	 	smallpic.attr("src","img/smallLight.svg")
 	}
 	smallClick++;
 });
@@ -246,52 +246,52 @@ $(".smallBrackets img").click(function() {
 
 $(".symm img").click(function() {
 	if (symClick%2 == 0){
-	 	sympic.attr("src","img/symm.png")
-	 	asympic.attr("src","img/asymmLight.png")
+	 	sympic.attr("src","img/symm.svg")
+	 	asympic.attr("src","img/asymmLight.svg")
 		asymClick =0;
 	}
 
 	else{
-	 	sympic.attr("src","img/symmLight.png")
+	 	sympic.attr("src","img/symmLight.svg")
 	}
 	symClick++;
 });
 
 $(".asymm img").click(function() {
 	if (asymClick%2 == 0){
-	 	asympic.attr("src","img/asymm.png")
-	 	sympic.attr("src","img/symmLight.png")
+	 	asympic.attr("src","img/asymm.svg")
+	 	sympic.attr("src","img/symmLight.svg")
 		symClick =0;
 	}
 
 	else{
-	 	asympic.attr("src","img/asymmLight.png")
+	 	asympic.attr("src","img/asymmLight.svg")
 	}
 	asymClick++;
 });
 
 $(".curve img").click(function() {
 	if (curveClick%2 == 0){
-	 	curvepic.attr("src","img/curve.png")
-	 	flatpic.attr("src","img/flatLight.png")
+	 	curvepic.attr("src","img/curve.svg")
+	 	flatpic.attr("src","img/flatLight.svg")
 		flatClick =0;
 	}
 
 	else{
-	 	curvepic.attr("src","img/curveLight.png")
+	 	curvepic.attr("src","img/curveLight.svg")
 	}
 	curveClick++;
 });
 
 $(".flat img").click(function() {
 	if (flatClick%2 == 0){
-	 	flatpic.attr("src","img/flat.png")
-	 	curvepic.attr("src","img/curveLight.png")
+	 	flatpic.attr("src","img/flat.svg")
+	 	curvepic.attr("src","img/curveLight.svg")
 		curveClick =0;
 	}
 
 	else{
-	 	flatpic.attr("src","img/flatLight.png")
+	 	flatpic.attr("src","img/flatLight.svg")
 	}
 	flatClick++;
 });
